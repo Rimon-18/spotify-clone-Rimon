d840cdafc880c3b509b51dfe2f64510209090edb
Rimon-18 <ratannandi143@gmail.com>
2026-05-07 01:50:01 +0530

    added server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve all static files
+app.use(express.static(path.join(__dirname)));
+
+// Serve songs folder
+app.use("/songs", express.static(path.join(__dirname, "songs")));
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
