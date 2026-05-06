5e60e4cb1c2842617dd8673f906bb045f560800b
Rimon-18 <ratannandi143@gmail.com>
2026-05-07 02:45:32 +0530

    fixed port 10000

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 10000;
 
 // Serve all static files
 app.use(express.static(path.join(__dirname)));
