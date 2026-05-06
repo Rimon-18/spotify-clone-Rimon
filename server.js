7a7ff7434a025b9bd361cf7beead1c65b0c89e82
Rimon-18 <ratannandi143@gmail.com>
2026-05-07 02:51:01 +0530

    fixed server binding

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 
 // Serve all static files
 app.use(express.static(path.join(__dirname)));
@@ -11,6 +11,11 @@ app.use(express.static(path.join(__dirname)));
 // Serve songs folder
 app.use("/songs", express.static(path.join(__dirname, "songs")));
 
-app.listen(PORT, () => {
+// Handle all routes
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
 });
